1077d87e769c8b55d16ddb586f6fca5814daf864
brandolkuete <brandolkuete95@gmail.com>
2022-02-20 01:02:36 +0100

    initializing the project

---
+++ src/test/java/com/brandolkuete/immobilier/ImmobilierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandolkuete.immobilier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobilierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
